27909b615cd0ec9d8a4d07500f4bbfcf45ec64b5
Quang Nguyen <nguyenquangv97@gmail.com>
2024-06-16 08:43:20 -0700

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: 'https://proud-cod-10.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+
+export default authConfig;
